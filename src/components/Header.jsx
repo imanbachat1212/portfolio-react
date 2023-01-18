7b094216a9aa58df878f273bcd59050b131da4e0
iman <iman.bachatt@gmail.com>
2023-01-18 23:44:41 +0200

    finalupdate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,23 @@ import './Header.css'
 const Header = () => {
   return (
     <>
-    <header id="header">
-   <img src={require('../images/signature.png')}  alt="" className='logo' />
+      <header>
+        <img src={require('../images/signature.png')} alt="" className='logo' />
 
-      <ul className="navlist   ">
-        <li><a href="#home" className="active"> Home</a></li>
-        <li><a href="#about"> About </a></li>
-        <li><a href="#work">Work</a></li>
-        <li><a href="#skill"> Skills</a></li>
-        <li><a href="#contact">Contact Me</a></li>
-      </ul>
+        <ul className="navlist">
+          <li><a href="#home" className="active"> Home</a></li>
+          <li><a href="#about"> About </a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#skill"> Skills</a></li>
+          <li><a href="#contact">Contact Me</a></li>
+        </ul>
 
-      <div className="bx bx-menu" id="menu-icon"></div>
-     
-   
-       
-      
-    </header>
+        <div className="bx bx-menu" id="menu-icon"></div>
+
+
+
+
+      </header>
     </>
   )
 }
