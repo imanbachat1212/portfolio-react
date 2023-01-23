13ef72178f3fd921d2818f460ba38a27126bd214
iman <iman.bachatt@gmail.com>
2023-01-23 20:30:52 +0200

    last updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useRef} from 'react'
 import './Header.css'
 
+
 const Header = () => {
-  const [menuopen,setMenuOpen]=useState(false);
-  const toggleMenu=()=>{
-    setMenuOpen(!menuopen)
-  }
+  const navRef = useRef();
+
+
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
+
   return (
     <>
       <header>
         <img src={require('../images/signature.png')} alt="" className='logo' />
 
-        <ul className="navlist">
-          <li><a href="#home" className="active"> Home</a></li>
-          <li><a href="#about"> About </a></li>
-          <li><a href="#work">Work</a></li>
-          <li><a href="#skill"> Skills</a></li>
-          <li><a href="#contact">Contact Me</a></li>
-        </ul>
-
-        <div className="bx bx-menu" id="menu-icon" onClick={toggleMenu}></div>
+        
+          
+            <ul className="navlist active" ref={navRef}>
+                 <li><a href="#home" > Home</a></li>
+                 <li><a href="#about"> About </a></li>
+                 <li><a href="#work">Work</a></li>
+                 <li><a href="#skill"> Skills</a></li>
+                 <li><a href="#contact">Contact Me</a></li>
+                 </ul>
+          
+       
+                 <div className="bx bx-menu " id="menu-icon"  onClick={showNavbar}></div>
+                
 
 
 
--- src/index.js
+++ src/index.js
@@ -11,23 +11,40 @@ root.render(
   </React.StrictMode>
 );
 
-const header = document.querySelector("header");
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 100);
-});
+// const header = document.querySelector("header");
+// window.addEventListener ("scroll", function() {
+// 	header.classList.toggle ("sticky", window.scrollY > 100);
+// });
+
+
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-
-menu.onclick = () => {
+menu.addEventListener = () => {
 	menu.classList.toggle('bx-x');
 	navlist.classList.toggle('open');
 };
 
-window.onscroll = () => {
+window.addEventListener = () => {
 	menu.classList.remove('bx-x');
 	navlist.classList.remove('open');
-};
+}
 
 
+
+
+// function buttonClicked(){
+//     console.log("Button clicked");
+// }
+
+// window.onload=function(){
+// let menu = document.querySelector('#menu-icon');
+// let navlist = document.querySelector('.navlist');
+// menu.addEventListener ("click", buttonClicked, true)
+
+// window.addEventListener = () => {
+	
+// 	menu.classList.remove('bx-x');
+// 	navlist.classList.remove('open');
+// };}
